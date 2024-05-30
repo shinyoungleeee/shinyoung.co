43bc874a930b118a81c826ebb78956629268d706
Shinyoung Lee <shinyounglee.315@gmail.com>
2024-05-29 21:58:49 -0400

    fix bad import

--- app/app/(dashboard)/layout.tsx
+++ app/app/(dashboard)/layout.tsx
@@ -1,16 +1,16 @@
 import { ReactNode, Suspense } from "react";
 import Profile from "@/components/profile";
-import Nav from "@/components/nav";
+import NavOld from "@/components/nav-old";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Nav>
+      <NavOld>
         <Suspense fallback={<div>Loading...</div>}>
           <Profile />
         </Suspense>
-      </Nav>
-      <div className="min-h-screen dark:bg-black sm:pl-60">{children}</div>
+      </NavOld>
+      <div className="min-h-screen sm:pl-60 dark:bg-black">{children}</div>
     </div>
   );
 }
