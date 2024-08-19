382e93207eb3a4b7765ac768bfdc53ae3f80f204
Shinyoung Lee <shinyounglee.315@gmail.com>
2024-08-18 23:11:50 -0400

    add lint-staged for prettier formatting

--- packages/logger/.eslintrc.js
+++ packages/logger/.eslintrc.js
@@ -1,11 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/index.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/index.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
   env: {
     jest: true,
   },
-};
+}
